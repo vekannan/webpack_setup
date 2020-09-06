295d64108341db411894cc39a79fa63d0df0df4b
Venkatesan Kannan <sharevenki@gmail.com>
2020-09-06 18:51:20 -0500

    webpack with htmlwebpackplugin

--- newWebpack/webpack.config.js
+++ newWebpack/webpack.config.js
@@ -1,32 +1,45 @@
 const path = require("path")
+const { config } = require("process")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
+const outputPath = path.resolve(__dirname,"dist");
+const source = path.resolve(__dirname,"src")
+
+const webpackConfig = {
+  entry: `${source}/index.js`,
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname,"dist")
+    path: outputPath,
   },
   resolve: {
     extensions: ['.js', '.jsx']
-},
-module: {
+  }, 
+  module: {
     rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_module/,
-            use: ['babel-loader']
-        },
-        { 
-            test: /\.svg$/, 
-            loader: 'svg-inline-loader' 
-        },
-        {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              'css-loader'
-            ]
-        }
+      {
+          test: /\.(js|jsx)$/,
+          exclude: /node_module/,
+          use: ['babel-loader']
+      },
+      { 
+          test: /\.svg$/, 
+          loader: 'svg-inline-loader' 
+      },
+      {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+      }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Venkat webpack experiment',
+    filename: "index.html",
+    template: 'src/index.html'
+  })]
 }
+
+
+module.exports = webpackConfig;
